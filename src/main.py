be24f66c995ce47ca72045977363951fcbd02820
fvinodh1978 <f.vinodhfranklin@gmail.com>
2024-03-24 10:34:41 -0400

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a+b
+
+def say_hello():
+    print("Hi, This is my first CircleCI Demo Program...")
+
+if __name__ == '__main__':
+    say_hello()
---
+++ test/main_test.py
@@ -0,0 +1,13 @@
+
+import sys
+
+# adding src to the system path
+sys.path.insert(0, 'C:\\Personal\\Vinodh\\Projects\\DevOps\\Workspace\\CircleCi-Demo\\src')
+from main import add
+
+def test_add():
+    assert add(5, 1)==6
+    print("Add Function Works Correctly")
+
+if __name__ =='__main__':
+    test_add()
