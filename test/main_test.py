ee5d977aa452a2651c737e4b1ae1485de2a6a0e6
fvinodh1978 <f.vinodhfranklin@gmail.com>
2024-03-27 16:35:33 -0400

    Added a New Function to Device 2 Numbers

--- test/main_test.py
+++ test/main_test.py
@@ -3,7 +3,7 @@ import sys
 
 # adding src to the system path
 sys.path.insert(0, '/home/circleci/circleci-python/src')
-from main import add, product
+from main import add, product, division
 
 def test_add():
     assert add(5, 5)==10
@@ -13,6 +13,11 @@ def test_product():
     assert product(5, 5)==25
     print("Add Test : product Function Works Correctly")
 
+def test_division():
+    assert product(5, 5)==1
+    print("Add Test : Division Function Works Correctly")    
+
 if __name__ =='__main__':
     test_add()
     test_product()
+    test_division()
