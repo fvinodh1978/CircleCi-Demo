1441724e630e3651552bb3ae53e3db6ea4755120
fvinodh1978 <f.vinodhfranklin@gmail.com>
2024-03-27 16:19:21 -0400

    Added a new function to multiply 2 numbers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 def add(a, b):
     return a+b
 
+def product(a, b):
+    return a+b
+
 def say_hello():
     print("Hi, This is my first CircleCI Demo Program...")
 
--- test/main_test.py
+++ test/main_test.py
@@ -9,5 +9,10 @@ def test_add():
     assert add(5, 5)==10
     print("Add Test : Add Function Works Correctly")
 
+def test_product():
+    assert add(5, 5)==25
+    print("Add Test : product Function Works Correctly")
+
 if __name__ =='__main__':
     test_add()
+    test_product()
